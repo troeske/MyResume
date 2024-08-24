f62f4b948d2fa7875404382f98e41d33f757e524
Thorsten Roeske <thorsten@wearonize.com>
2024-08-24 16:40:59 +0000

    fix: removed console.log from sendMail

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,8 +1,4 @@
 function sendMail(contactForm) {
-    console.log(contactForm.fullname.value);
-    console.log(contactForm.emailaddress.value);
-    console.log(contactForm.projectsummary.value);
-    
     emailjs.send("service_hkaqw0i","template_jmch9w5", {
         "from_name": contactForm.fullname.value,
         "from_email": contactForm.emailaddress.value,
